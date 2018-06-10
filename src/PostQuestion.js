a71c08058597eddcea5eede4ed1c482412645891
MiniRaccoonDog <atorres@spoonflower.com>
2018-06-10 09:35:26 -0400

    added more details on Registration page, tentative actual functionality as well

--- src/PostQuestion.js
+++ src/PostQuestion.js
@@ -1,10 +1,11 @@
-/* global localStorage */
+/// * global localStorage */
 import React, { Component } from 'react'
 import './App.css'
 import request from 'superagent'
-import moment from 'moment'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import moment from 'moment'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {Breadcrumbs, BreadcrumbItem, Button} from 'react-foundation'
+import IndividualQuestionAndAnswers from './IndividualQuestionAndAnswers'
 
 class PostQuestion extends Component {
   constructor (props) {
@@ -71,31 +72,32 @@ class PostQuestion extends Component {
       return (
         <IndividualQuestionAndAnswers questionId={this.questionId} />)
     } else {
-    return (
-      <div>
+      return (
         <div>
-          <header>
-            <Breadcrumbs className='nav'>
-              <BreadcrumbItem><a href='/'><img className='nav_img' src='https://tinyurl.com/yb7ek22r' /></a></BreadcrumbItem>
-              {/* <BreadcrumbItem><a href='/'><img className='nav_img' src='./images/whatisit.png' /></a></BreadcrumbItem> */}
-              <BreadcrumbItem className='nav-center'><a href='/user/id'>My Questions</a></BreadcrumbItem>
-              <BreadcrumbItem className='nav-center'><a href='/questions/qid?'>Last Q</a></BreadcrumbItem>
-              <BreadcrumbItem className='nav-center'><a href='/??'>Logout</a></BreadcrumbItem>
-            </Breadcrumbs>
-          </header>
-        </div>
-        <div className='fullcenter'>
-          <h2 className='header'>Post a Question</h2>
-          <form className='postQuestionForm' type='submit' onSubmit={this.handleSubmit}>
+          <div>
+            <header>
+              <Breadcrumbs className='nav'>
+                <BreadcrumbItem><a href='/'><img className='nav_img' src='https://tinyurl.com/yb7ek22r' /></a></BreadcrumbItem>
+                {/* <BreadcrumbItem><a href='/'><img className='nav_img' src='./images/whatisit.png' /></a></BreadcrumbItem> */}
+                <BreadcrumbItem className='nav-center'><a href='/user/id'>My Questions</a></BreadcrumbItem>
+                <BreadcrumbItem className='nav-center'><a href='/questions/qid?'>Last Q</a></BreadcrumbItem>
+                <BreadcrumbItem className='nav-center'><a href='/??'>Logout</a></BreadcrumbItem>
+              </Breadcrumbs>
+            </header>
+          </div>
+          <div className='fullcenter'>
+            <h2 className='header'>Post a Question</h2>
+            <form className='postQuestionForm' type='submit' onSubmit={this.handleSubmit}>
             Title: <input type='text' name='title' onChange={this.handleChange} />
             Content: <textarea className='content-textarea' type='textarea' name='content' onChange={this.handleChange} />
             Photo URL: <input type='url' name='image' onChange={this.handleImage} />
-            <Button className='submitButton' type='submit'>Submit</Button>
-            <Button className='cancelButton' onClick={this.props.cancelSubmit} isHollow >Cancel</Button>
-          </form>
+              <Button className='submitButton' type='submit'>Submit</Button>
+              <Button className='cancelButton' onClick={this.props.cancelSubmit} isHollow >Cancel</Button>
+            </form>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {Button} from 'react-foundation'
-// import request from 'superagent'
+import {Button, Callout, Colors} from 'react-foundation'
+import request from 'superagent'
+import './foundation.css'
 
 class Register extends Component {
   constructor () {
@@ -9,7 +10,8 @@ class Register extends Component {
       username: '',
       password: '',
       passwordDup: '',
-      token: ''
+      token: '',
+      registrationFail: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.attemptLogin = this.attemptLogin.bind(this)
@@ -18,8 +20,28 @@ class Register extends Component {
     this.setState({[event.target.name]: event.target.value})
   }
 
+  doNOthing (event) {
+    event.preventDefault()
+  }
+
   attemptLogin () {
     console.log('register attempted')
+    if (this.state.password === this.state.passwordDup) {
+      request
+        .post(`localhost:3000/api/v1/users`)
+        // .set('Content-Type', 'application/json')
+        .send({
+          'username': this.state.username,
+          'password': this.state.password
+        })
+        .then((response) => {
+          window.localStorage.token = response
+          this.setState({token: response})
+          this.setState({registrationFail: false})
+        })
+    } else {
+      this.setState({registrationFail: true})
+    }
   }
   render () {
     return (
@@ -32,9 +54,13 @@ class Register extends Component {
           <input type='text' name='password' placeholder='create your password' onChange={(event) => this.handleChange(event)} />
           <label>Password Confirmation</label>
           <input type='text' name='passwordDup' placeholder='confirm your password' onChange={(event) => this.handleChange(event)} />
-          {this.state.password === this.state.passwordDup && <div className='input-success'>Your Passwords Match!</div>}
+          {this.state.password !== '' && this.state.password === this.state.passwordDup && <div className='input-success'>Your Passwords Match!</div>}
           {this.state.password && this.state.passwordDup && this.state.password !== this.state.passwordDup && <div className='input-warning'>Passwords Do Not Match!</div>}
-          <Button isExpanded type='submit' >Submit</Button>
+          {this.state.password !== '' && this.state.password === this.state.passwordDup ? <Button isExpanded>Submit</Button> : <Button isExpanded isDisabled onClick={(event) => this.doNOthing(event)}>Submit</Button>}
+          {this.state.registrationFail && <Callout color={Colors.ALERT} className='text-center'>
+            <h5>Unable to Register</h5>
+            <p>Username taken, please choose another</p>
+          </Callout>}
         </form>
       </div>
     )
